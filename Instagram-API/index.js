bdb95657d9937745e7f5af934dc5e7ed0755c3ef
Rohit Sridharan <79148508+rohit25-1@users.noreply.github.com>
2023-12-14 09:07:06 +0530

    Added Middleware

--- Instagram-API/index.js
+++ Instagram-API/index.js
@@ -6,6 +6,8 @@ require("./db/connect");
 
 const user = require("./models/user");
 const post = require("./models/post");
+const auth = require("./middleware/auth");
+const authMiddleware = require("./middleware/auth");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,12 +34,50 @@ app.post("/api/login", async (req, res) => {
     const findUser =
       (await user.findOne({ username, password })) ||
       (await user.findOne({ email, password }));
-    if (findUser) res.status(200).json(findUser);
-    else {
+    if (findUser) {
+      const token = jwt.sign(findUser.id, "SECRET");
+      res.header("Authorization", token);
+      res.status(200).json({ Success: "OK" });
+    } else {
       res.status(400).json({ error: "check username or password" });
     }
   } catch (error) {
     console.log(error);
+  }
+});
+
+app.get("/api/islogged", async (req, res) => {
+  const headers = req.headers["authorization"];
+  if (headers) {
+    const id = jwt.verify(headers, "SECRET");
+    const isUser = await user.findOne({
+      id,
+    });
+    if (isUser) {
+      res.status(200).json({ success: 1 });
+    } else {
+      res.status(400).json({ success: 0 });
+    }
+  }
+});
+
+app.get("/api/users/:userId", authMiddleware, async (req, res) => {
+  try {
+    if (req.params.userId !== req.user) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: You can only access your own details" });
+    }
+
+    const findUser = await user.findById(req.params.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ findUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
---
+++ Instagram-API/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async function (req, res, next) {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: Missing token" });
+  }
+
+  jwt.verify(token, "SECRET", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+    console.log(decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = authMiddleware;
